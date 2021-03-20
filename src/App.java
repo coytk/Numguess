5b2d1234b71575a65488a07d6e01a215db82e799
coytk <coy19950407@gmail.com>
2021-03-20 23:13:49 +0900

    init

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        while(true){
+            int number = (int) (Math.random() * 2);
+            System.out.println("first: "+number);
+            int a;
+            a=0%3;
+            System.out.println(a);
+
+
+            try{
+                Thread.sleep(500);
+            }catch(InterruptedException e){
+            }
+        }
+        
     }
 }
